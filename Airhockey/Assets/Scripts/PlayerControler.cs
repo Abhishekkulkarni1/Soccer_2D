863c4342492e233644173cceb341dd1dc5051b19
roman <roman.safron@aon.at>
2020-11-30 10:39:54 +0100

    initial commit
    gg

---
+++ Airhockey/Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    public Transform cameraPosition;
+    public LayerMask layerTable;
+    private Vector3 cameraVector;
+    private Vector3 lastKnown;
+    
+
+    void FixedUpdate()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        
+        if (Physics.Raycast(ray, out hit, 100, layerTable))
+        {
+            Debug.DrawLine(ray.origin, hit.point, Color.red);
+            transform.position = hit.point;
+            lastKnown = new Vector3(transform.position.x, transform.position.y,transform.position.z );
+        }
+        else
+        {
+            Debug.DrawLine(ray.origin, ray.origin + ray.direction*100f , Color.blue);
+            transform.position = lastKnown;
+        }
+        
+    }
+}
